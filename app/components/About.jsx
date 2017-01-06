af0f4d3e80ce6ed74fa39257acb98d4f2e2a62da
Alexey Kurdyukov <struan.dirk.29@gmail.com>
2017-01-06 14:44:01 +0300

    About page has been designed

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,15 @@ var React = require('react');
 var About = React.createClass({
     render: function(){
         return (
-            <h3>About component</h3>
+            <div>
+                <h1 className="text-center">About</h1>
+                <p>This is education application for React. Allows to fetch data from "Open weather map"</p>
+                <ol>
+                    <li>Link to resource those provide weather information <a href="http://openweathermap.org/">Open weather map</a></li>
+                    <li><a href="https://github.com/facebook/react">Github</a> repository</li>
+                    <li><a href="https://www.udemy.com/">Udemy</a>. Education resource, has been created React education course</li>
+                </ol>
+            </div>
         )
     }
 })
